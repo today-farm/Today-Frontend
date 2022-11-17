36962b2f38deece6646f29689246a94dada1aeba
홍희선 <hia990618@gmail.com>
2022-11-17 23:51:32 +0900

    feat: 유저 정보 변경 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CookiesProvider, useCookies } from "react-cookie";
 import Login from "./components/Login";
-import Signup from "./components/FormSignup";
+import Signup from "./components/Signup";
 import Withdraw from "./components/Withdraw";
+import TodayPost from "./components/Today";
+import Edit from "./components/Edit";
 
 function App() {
   // const [cookies, removeCookie] = useCookies(["accessToken"]);
@@ -26,13 +28,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/withdraw" element={<Withdraw />} />
           <Route path="/" element={<p>메인페이지입니다.</p>} />
-          {/* 유저 정보 페이지 */}
-          {/* {cookies.email ? (
-            <Route path="/widthDraw" element={<Withdraw />} />
-          ) : (
-            <Route path="/login" element={<Login />} />
-          )} */}
-          {/* // */}
+          <Route path="/post" element={<TodayPost />} />
+          <Route path="/edit" element={<Edit />} />
           <Route path="*" element={<p>여기는 없는 페이지입니다😢</p>} />
         </Routes>
       </BrowserRouter>
