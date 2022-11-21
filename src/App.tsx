0729eba44a78e0a9ce02a0ea0ff4b1f258ee7e25
홍희선 <hia990618@gmail.com>
2022-11-22 01:51:56 +0900

    feat: Today create 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { CookiesProvider, useCookies } from "react-cookie";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Withdraw from "./components/Withdraw";
+import TodayPost from "./components/Today";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/withdraw" element={<Withdraw />} />
+          <Route path="/post" element={<TodayPost />} />
           <Route path="/" element={<p>메인페이지입니다.</p>} />
           <Route path="*" element={<p>여기는 없는 페이지입니다😢</p>} />
         </Routes>
