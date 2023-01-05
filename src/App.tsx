a3888caa546a937fc33b69141147dc82dd01ac03
홍희선 <hia990618@gmail.com>
2023-01-05 18:54:12 +0900

    refector: UI에 관련없는 상위 컴포넌트 파일 옮김

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import React, { useRef, useState, useEffect } from "react";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { CookiesProvider, useCookies } from "react-cookie";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import Withdraw from "./components/Withdraw";
-import TodayPost from "./components/Today";
-import EditAccount from "./components/EditAccount";
-import FriendAdd from "./components/Friendadd";
-
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import Login from './components/Login';
+import Signup from './components/Signup/Signup';
+import Withdraw from './components/Withdraw';
+import TodayPost from './components/Today';
+import EditAccount from './components/EditAccount';
+import FriendAdd from './components/Friendadd';
 
 function App() {
   return (
     <CookiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/withdraw" element={<Withdraw />} />
-          <Route path="/post" element={<TodayPost />} />
-          <Route path="/edit" element={<EditAccount />} />
-          <Route path="/friend/add" element={<FriendAdd />} />
-
-          <Route path="/" element={<p>메인페이지입니다.</p>} />
-          <Route path="/post" element={<TodayPost />} />
-          {/* <Route path="/edit" element={<Edit />} /> */}
-          <Route path="*" element={<p>여기는 없는 페이지입니다😢</p>} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/withdraw' element={<Withdraw />} />
+        <Route path='/post' element={<TodayPost />} />
+        <Route path='/edit' element={<EditAccount />} />
+        <Route path='/friend/add' element={<FriendAdd />} />
+        <Route path='/' element={<p>메인페이지입니다.</p>} />
+        <Route path='/post' element={<TodayPost />} />
+        <Route path='*' element={<p>여기는 없는 페이지입니다😢</p>} />
+      </Routes>
     </CookiesProvider>
   );
 }
---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,80 @@
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyle = createGlobalStyle`
+
+:root {
+  /*       [ color ]     */
+  --black: #1D1F00;
+  --light-gray: rgba(29, 31, 0, 0.2);
+  --dark-green: #41731D;
+  --green: #79A938;
+  --mid-green: #BCDD83;
+  --light-green: #E2F0C9;
+  --more-light-green: #F4F9E9;
+  --red: #F06643;
+  --dark-beige: #938570;
+  --light-beige: #BDAC93;
+  --light-yellow: #FFFAE0;
+  --light-orange: #FFD443;
+  --orange: #F2AE27;
+  --light-blue: #71ABD2;
+  --blue: #3C7DB6;
+  --dark-blue: #2A5D93;
+
+
+  /*       [ font-size ]     */
+  --more-big: 28px;
+  --big: 24px;
+  --mid: 20px;
+  --mid-lineHeight: 24px;
+  --small: 16px;
+  --small-lineHeight-1: 20px;
+  --small-lineHeight-2: 28px;
+  --more-small: 14px;
+}
+
+html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+    box-sizing: border-box;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
