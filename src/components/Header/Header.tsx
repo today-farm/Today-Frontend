a94175c1aa56143f315099246656fff38bbf589e
홍희선 <hia990618@gmail.com>
2023-01-11 00:34:36 +0900

    feat: navigate 버튼 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { HeaderWrapper, BackIcon, Title } from './style';
-
+import { useNavigate } from "react-router-dom";
 interface Iprops {
   title: string;
 }
 
 export default function Header(props: Iprops) {
+  let navigate = useNavigate();
+  const handleClick = () =>{
+    navigate(-1);
+  }
   return (
     <HeaderWrapper>
-      <BackIcon alt='icon_back' src='/img/icon_back.png' />
+      <BackIcon alt='icon_back' src='/img/icon_back.png' onClick={handleClick}/>
       <Title>{props.title}</Title>
     </HeaderWrapper>
   );
