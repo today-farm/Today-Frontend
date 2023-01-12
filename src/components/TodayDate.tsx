681b34966a3ea839550a412bf5508bf9b94c9521
widcellent <76591065+widcellent@users.noreply.github.com>
2023-01-12 16:47:59 +0900

    오늘 날짜 컴포넌트 추가

---
+++ src/components/TodayDate.tsx
@@ -0,0 +1,14 @@
+export function TodayDate () {
+    const current = new Date();
+    const week = ['일','월','화','수','목','금','토'];
+    let dayOfWeek = week[current.getDay()];
+
+    const date = `${current.getFullYear()}년 ${current.getMonth()+1}월 ${current.getDate()}일 
+    ${dayOfWeek}요일`;
+    
+    return(
+        <>
+            <h1>{date}</h1>
+        </>
+    )
+}
