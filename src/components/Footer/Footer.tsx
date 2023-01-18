eafe28f37cfb52a3281edbf98889cb1e72b33fa4
홍희선 <hia990618@gmail.com>
2023-01-18 23:09:49 +0900

    design: main farm markup 진행 중

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,14 +5,31 @@ import {
   FarmIconWrapper,
   FarmIcon,
   Icon,
+  RabbitImg,
+  TextBox,
+  TextWrapper,
+  Text,
 } from './style'
-function Footer() {
+interface Iprops {
+  main?: boolean
+}
+function Footer(props: Iprops) {
   return (
     <FooterWrapper>
+      {props.main && (
+        <TextWrapper>
+          <TextBox src="/img/Union.png" />
+          <Text>오늘 하루는 어땠나요?</Text>
+        </TextWrapper>
+      )}
       <FarmIconWrapper>
-        <FarmIcon>
-          <img src="/img/icons/icon_farm.png" />
-        </FarmIcon>
+        {props.main ? (
+          <RabbitImg src="/img/character/mainRabbit.png" />
+        ) : (
+          <FarmIcon>
+            <img src="/img/icons/icon_farm.png" />
+          </FarmIcon>
+        )}
       </FarmIconWrapper>
       <IconWrapper>
         <Icon src="/img/icons/icon_calendar.png" />
--- src/components/MainFarm/MainFarm.tsx
+++ src/components/MainFarm/MainFarm.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { GreenComponentWrapper } from '../../style/CommonStyles'
 import Footer from './../Footer/Footer'
+import { todayYearMonthDate } from '../Today/TodayDate'
+import { Title } from '../../style/CommonStyles'
+import Field from './Field/Field'
 
 function MainFarm() {
   return (
     <GreenComponentWrapper>
-      <Footer />
+      <Title>{todayYearMonthDate()}</Title>
+      <Field text={'첫 번째 하루를 기록해보세요.'} />
+      <Field text={'8개의 기록이 필요해요.'} />
+      <Field text={'15개의 기록이 필요해요.'} />
+      <Field text={'22개의 기록이 필요해요'} />
+
+      <Footer main={true} />
     </GreenComponentWrapper>
   )
 }
